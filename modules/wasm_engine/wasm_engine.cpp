7997da2d3de5b48b56be8adcd39bd69884bf3df4
Shane Liesegang <shane@techie.net>
2022-04-02 13:58:24 +0300

    loading basic wat from file

--- modules/wasm_engine/wasm_engine.cpp
+++ modules/wasm_engine/wasm_engine.cpp
@@ -24,7 +24,8 @@ static wasm_trap_t* callback(
 }
 
 /* static */ void WasmProgram::_bind_methods() {
-	// ClassDB::bind_method(D_METHOD("init_engine"), &WasmProgram::init_engine);
+	ClassDB::bind_method(D_METHOD("load_wat", "wat_text"), &WasmProgram::load_wat);
+	ClassDB::bind_method(D_METHOD("run_wat"), &WasmProgram::run_wat);
 }
 
 /* static */ void WasmProgram::_print_wasmtime_error(const char* msg, wasmtime_error_t *err, wasm_trap_t *trap) {
@@ -46,67 +47,81 @@ WasmProgram::WasmProgram() {
 		_engine = wasm_engine_new();
 	}
 	_programCount += 1;
-	this->wasm_store = wasmtime_store_new(_engine, NULL, NULL);
-	wasmtime_context_t* context = wasmtime_store_context(this->wasm_store);
+	this->store = wasmtime_store_new(_engine, NULL, NULL);
+	this->module = NULL;
+}
 
-	const char* wat =
-		"(module"
-			"(func $call_to_engine (import \"\" \"call_to_engine\"))"
-			"(func (export \"run\") (call $call_to_engine))"
-		")";
+WasmProgram::~WasmProgram() {
+	if (this->module != NULL) {
+		wasmtime_module_delete(this->module);
+		this->module = NULL;
 
+	}
+	wasmtime_store_delete(this->store);
+	this->store = NULL;
+
+	_programCount -= 1;
+	if (_programCount == 0) {
+		wasm_engine_delete(_engine);
+		_engine = NULL;
+	}
+}
+
+void WasmProgram::load_wat(String wat_text) {
 	wasm_byte_vec_t wasm;
-	wasmtime_error_t* err = wasmtime_wat2wasm(wat, strlen(wat), &wasm);
+	CharString wat_text_str = wat_text.utf8();
+	wasmtime_error_t* err = wasmtime_wat2wasm(wat_text_str.get_data(), wat_text_str.length(), &wasm);
 	if (err != NULL) {
 		WasmProgram::_print_wasmtime_error("failed to parse wat", err, NULL);
 		return;
 	}
 
-	wasmtime_module_t* module = NULL;
-	err = wasmtime_module_new(_engine, (uint8_t*)wasm.data, wasm.size, &module);
+	if (this->module != NULL) {
+		wasmtime_module_delete(this->module);
+		this->module = NULL;
+	}
+	err = wasmtime_module_new(_engine, (uint8_t*)wasm.data, wasm.size, &this->module);
 	wasm_byte_vec_delete(&wasm);
 	if (err != NULL) {
 		WasmProgram::_print_wasmtime_error("failed to compile module", err, NULL);
 		return;
 	}
 
+	wasmtime_context_t* context = wasmtime_store_context(this->store);
+
 	wasm_functype_t *test_msg = wasm_functype_new_0_0();
 	wasmtime_func_t test;
 	wasmtime_func_new(context, test_msg, callback, NULL, NULL, &test);
 
 	wasm_trap_t* trap = NULL;
-	wasmtime_instance_t instance;
 	wasmtime_extern_t import;
 	import.kind = WASMTIME_EXTERN_FUNC;
 	import.of.func = test;
-	err = wasmtime_instance_new(context, module, &import, 1, &instance, &trap);
+	err = wasmtime_instance_new(context, this->module, &import, 1, &this->instance, &trap);
 	if (err != NULL || trap != NULL) {
 		WasmProgram::_print_wasmtime_error("failed to instantiate", err, trap);
 		return;
 	}
+}
+
+void WasmProgram::run_wat() {
+	if (this->module == NULL) {
+		print_error("Cannot call 'run_wat' before loading program.");
+		return;
+	}
+	wasmtime_context_t* context = wasmtime_store_context(this->store);
 
 	wasmtime_extern_t run;
-	bool ok = wasmtime_instance_export_get(context, &instance, "run", 3, &run);
+	bool ok = wasmtime_instance_export_get(context, &this->instance, "run", 3, &run);
 	if (!ok || run.kind != WASMTIME_EXTERN_FUNC) {
 		print_error("Could not find 'run' function in WASM.");
 		return;
 	}
 
-	err = wasmtime_func_call(context, &run.of.func, NULL, 0, NULL, 0, &trap);
+	wasm_trap_t* trap = NULL;
+	wasmtime_error_t* err = wasmtime_func_call(context, &run.of.func, NULL, 0, NULL, 0, &trap);
 	if (err != NULL || trap != NULL) {
 		WasmProgram::_print_wasmtime_error("failed to call function", err, trap);
 		return;
-	}
-
-	wasmtime_module_delete(module);
-}
-
-WasmProgram::~WasmProgram() {
-	wasmtime_store_delete(this->wasm_store);
-
-	_programCount -= 1;
-	if (_programCount == 0) {
-		wasm_engine_delete(_engine);
-		_engine = NULL;
 	}
 }
--- modules/wasm_engine/wasm_engine.h
+++ modules/wasm_engine/wasm_engine.h
@@ -13,11 +13,16 @@ protected:
 	static void _print_wasmtime_error(const char* msg, wasmtime_error_t* err, wasm_trap_t *trap);
 
 private:
-	wasmtime_store_t* wasm_store;
+	wasmtime_store_t* store;
+	wasmtime_module_t* module;
+	wasmtime_instance_t instance;
 
 public:
 	WasmProgram();
 	~WasmProgram();
+
+	void load_wat(String wat_text);
+	void run_wat();
 };
 
 
